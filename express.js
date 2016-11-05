e973438e411fb270b228543b0c662133200d92f6
tomikpik <tomas.pikous@gmail.com>
2016-11-05 20:02:09 +0100

    added cors

--- express.js
+++ express.js
@@ -2,6 +2,13 @@ var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.get('/:id', function (req, res) {
     MongoClient.connect("mongodb://iot.eclubprague.com:27017/traq", function (err, db) {
         if (!err) {
